04805ce888fe18f2f038db3961f64e9f9448596f
Antoine Sauvage <antoine@ovrsea.com>
2023-03-12 18:31:31 +0100

    chore: working script

--- run.sh
+++ run.sh
@@ -1,11 +1,3 @@
-### Install Apprise
-
-`pip install apprise`
-
-
-### Start changedetection.io
-
-```
 docker build -t rwc .
 docker run -d --restart always -p "127.0.0.1:5001:5001" -v datastore-volume:/datastore --name rwc rwc 
 docker kill rwc
@@ -13,4 +5,3 @@ export BACKUP_PATH=~/changedetection.io/world-cup.zip
 docker run -v datastore-volume:/datastore -v $BACKUP_PATH:/backup.zip python:3.8-slim bash -c "apt-get update && apt-get install unzip -y; unzip -o -j /backup.zip -d /datastore"
 docker restart rwc
 open "127.0.0.1:5001"
-```
