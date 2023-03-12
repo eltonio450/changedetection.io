c899e14296c9610d8ef8f87c0d277ff8c144466b
Antoine Sauvage <antoine@ovrsea.com>
2023-03-12 22:30:52 +0100

    fix: docker

--- run.sh
+++ run.sh
@@ -1,7 +1,4 @@
 docker build -t rwc .
-docker run -d --restart always -p "127.0.0.1:5001:5001" -v datastore-volume:/datastore --name rwc rwc 
-docker kill rwc
-export BACKUP_PATH=~/changedetection.io/world-cup/
-docker run -v datastore-volume:/datastore -v $BACKUP_PATH:/world-cup python:3.8-slim bash -c "cp world-cup/* -d /datastore"
-docker restart rwc
+docker run -v datastore-volume:/datastore -v ~/changedetection.io/world-cup/:/world-cup python:3.8-slim bash -c "cp world-cup/* -d /datastore"
+docker run -d --restart always -p "5001:5001" -v datastore-volume:/datastore --name rwc rwc 
 open "http://127.0.0.1:5001"
