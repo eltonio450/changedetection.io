09e18d15cecabd91f3b8a497dca7c00ae8433202
Antoine Sauvage <antoine@ovrsea.com>
2023-03-12 21:39:00 +0100

    script: run

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ docker kill rwc
 export BACKUP_PATH=~/changedetection.io/world-cup.zip
 docker run -v datastore-volume:/datastore -v $BACKUP_PATH:/backup.zip python:3.8-slim bash -c "apt-get update && apt-get install unzip -y; unzip -o -j /backup.zip -d /datastore"
 docker restart rwc
-open "127.0.0.1:5001"
+open "http://127.0.0.1:5001"
