07651cd26adf5d60a09ce5fec12c858aca0c1862
Antoine Sauvage <antoine@ovrsea.com>
2023-03-13 21:00:37 +0100

    chore: add cron

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+bash cron.sh
 docker build -t rwc .
 docker run -v datastore-volume:/datastore -v ~/changedetection.io/world-cup/:/world-cup python:3.8-slim bash -c "cp world-cup/* -d /datastore"
 docker run -d --restart always -p "5001:5001" -v datastore-volume:/datastore --name rwc rwc 
