8ef6c78765993b7b349e16bd8c2a66b8dbbd5dd5
Antoine Sauvage <antoine@ovrsea.com>
2023-03-14 08:29:08 +0100

    chore: source + pause

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 bash cron.sh
 docker build -t rwc .
+docker run -d --name selenium --restart unless-stopped -p 4444:4444 --shm-size="2g" seleniarm/standalone-chromium:4.0.0-20211213
 docker run -v datastore-volume:/datastore -v ~/changedetection.io/world-cup/:/world-cup python:3.8-slim bash -c "cp world-cup/* -d /datastore"
-docker run -d --restart always -p "5001:5001" -v datastore-volume:/datastore --name rwc rwc 
+docker run -d --restart always --link selenium -p "5001:5001" -e WEBDRIVER_URL="http://selenium:4444/wd/hub" -v datastore-volume:/datastore --name rwc rwc 
