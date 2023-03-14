50700e2b02a8747b2dd7cc02fa2de03f74c296b4
Antoine Sauvage <antoine@ovrsea.com>
2023-03-14 10:11:48 +0100

    chore: add cookie and bench

--- bench/main.js
+++ bench/main.js
@@ -13,7 +13,11 @@ const promise = fetch("https://tickets.rugbyworldcup.com/fr/revente_france_urugu
     "sec-fetch-mode": "navigate",
     "sec-fetch-site": "none",
     "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1"
+    "upgrade-insecure-requests": "1",
+    "referer": "https://tickets.rugbyworldcup.com/fr",
+    "referrer-policy": "strict-origin-when-cross-origin",
+    "cookie": "SSESSd01ad0e956e3c977e4fabd0c735f147f=M4aodvR8cJn1SKaRbkDF9tPQ3tlj6VLYbaj3TKURjnz5nYe3;"
+
   },
   "referrer": "https://tickets.rugbyworldcup.com/fr",
   "referrerPolicy": "strict-origin-when-cross-origin",
@@ -25,6 +29,7 @@ const promise = fetch("https://tickets.rugbyworldcup.com/fr/revente_france_urugu
   
 promise.then(console.log)
 
-promise.then(response => response.json())
+promise.then(response => response.text())
 .then(data => console.log(data))
 .catch(error => console.error(error));
+
