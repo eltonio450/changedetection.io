b7c6a69577b68250f3633ff5781d95891c0c3c91
Antoine Sauvage <antoine@ovrsea.com>
2023-03-14 09:12:27 +0100

    remove cron

--- cron.sh
+++ cron.sh
@@ -1,2 +1,3 @@
-echo "26,56 * * * *  docker restart rwc" | crontab -
-crontab -l | { cat; echo "4,34 * * * *  docker stop rwc"; } | crontab -
+echo "" | crontab -
+#echo "26,56 * * * *  docker restart rwc" | crontab -
+#crontab -l | { cat; echo "4,34 * * * *  docker stop rwc"; } | crontab -
