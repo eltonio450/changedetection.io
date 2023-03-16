af8f4c7f4f35b8e15b217ba8273434d38fc74e2f
Antoine Sauvage <antoine@ovrsea.com>
2023-03-16 16:47:24 +0100

    chore: adding page

--- book/src/extract.test.ts
+++ book/src/extract.test.ts
@@ -1,4 +1,20 @@
+import * as fs from "fs"
+import { pageToBody } from "./page-to-request"
+
 it("test", () => {
-    expect(1).toBe(1)
+
+    const page = fs.readFileSync("./data/ang-arg/page.html").toString()
+    console.log(page)
+    expect(pageToBody(page)).toEqual({
+        "nb_tickets_selector": {
+            "quantity": "0"
+          },
+          "tickets_selected": "",
+          "form_build_id": "form-9cNWwfHNR75CVKgFrqWLpoIpSal6Y37RfsgaViADUC0",
+          "form_token": "5vMNSg5zQaYnkhDEP6FvZzDdhMCEvHRcXgL_t57qttY",
+          "form_id": "hubber_resale_add_to_cart_form",
+          "_triggering_element_name": "add_pack31672",
+          "_triggering_element_value": "Ajouter à mon panier",
+    })
 }
 )
--- book/src/page-to-request.ts
+++ book/src/page-to-request.ts
@@ -1,13 +1,38 @@
+var HTMLParser = require('node-html-parser');
+
+
 export type NoSelectionBody = {
     "nb_tickets_selector": {
         "quantity": "0"
       },
       "tickets_selected": "",
-      "form_build_id": "form-9cNWwfHNR75CVKgFrqWLpoIpSal6Y37RfsgaViADUC0",
-      "form_token": "5vMNSg5zQaYnkhDEP6FvZzDdhMCEvHRcXgL_t57qttY",
-      "form_id": "hubber_resale_add_to_cart_form",
-      "_triggering_element_name": "add_pack31672",
+      "form_build_id": string,
+      "form_token": string,
+      "form_id": string,
+      "_triggering_element_name": string,
       "_triggering_element_value": "Ajouter à mon panier",
 }
 
-const pageToBody = (page: string): 
+export const pageToBody = (page: string): NoSelectionBody => {
+
+    const root = HTMLParser.parse(page);
+
+    const regex = /"_triggering_element_name":"([a-zA-Z_0-9]+)"/;
+    const match = regex.exec(page);
+
+    const form_token = root.querySelector('input[name="form_token"]')._attrs.value;
+    const form_id = root.querySelector('input[name="form_id"]')._attrs.value;
+    const form_build_id = root.querySelector('input[name="form_build_id"]')._attrs.value;
+    return ({
+        form_token,
+        form_id,
+        form_build_id,
+        //@ts-ignore
+        _triggering_element_name: match[1],
+        "nb_tickets_selector": {
+            "quantity": "0"
+          },
+          "tickets_selected": "",
+          "_triggering_element_value": "Ajouter à mon panier",
+    })
+}
