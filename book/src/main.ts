a5595afc355daef7c7ee60b816bf977db48cbf62
Antoine Sauvage <antoine@ovrsea.com>
2023-03-16 18:44:40 +0100

    ok almost working

--- book/src/main.ts
+++ book/src/main.ts
@@ -1,10 +1,44 @@
 import { fetchMainPage } from "./fetch-main-page";
+import { noSelectionPageToBody } from "./page-handler/page-to-form-body";
+import * as fs from "fs"
 
-const token = "";
-const url = "";
+const token = "SSESSd01ad0e956e3c977e4fabd0c735f147f=uN9K4PFY5ZrjjQsnfz45t1PJ8LpJ4VlXvPL%2CcgzanMfk4CTB";
+const url = "https://tickets.rugbyworldcup.com/fr/revente_france_namibie";
+
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+})
+
+
 
 const main = async () => {
-  const initialPage = fetchMainPage(url, token);
+
+  //const initialPage = await (await fetchMainPage(url, token)).text();
+  //fs.writeFileSync("log.html", initialPage)
+  //const body = noSelectionPageToBody(initialPage)
+  
+  //console.log(body)
+  
+
 };
 
-main().then(console.log);
+
+// Declare a route
+fastify.get('/', function (request, reply) {
+    reply.send({ hello: 'world' })
+  })
+
+  fastify.post('/', function (request, reply) {
+    console.log(request)
+    reply.send({ hello: 'world' })
+  })
+  
+  // Run the server!
+  fastify.listen({ port: 5002 }, function (err, address) {
+    if (err) {
+      fastify.log.error(err)
+      process.exit(1)
+    }
+    // Server is now listening on ${address}
+  })
---
+++ book/src/page-handler/all.test.ts
@@ -0,0 +1,36 @@
+import * as fs from "fs";
+import { noSelectionPageToBody } from "./page-to-form-body";
+
+it("no selection page", () => {
+  const page = fs.readFileSync("./data/ang-arg/page.html").toString();
+  console.log(page);
+  expect(noSelectionPageToBody(page)).toEqual({
+    nb_tickets_selector: {
+      quantity: "0",
+    },
+    tickets_selected: "",
+    form_build_id: "form-9cNWwfHNR75CVKgFrqWLpoIpSal6Y37RfsgaViADUC0",
+    form_token: "5vMNSg5zQaYnkhDEP6FvZzDdhMCEvHRcXgL_t57qttY",
+    form_id: "hubber_resale_add_to_cart_form",
+    _triggering_element_name: "add_pack31672",
+    _triggering_element_value: "Ajouter à mon panier",
+  });
+});
+
+it("selection page", () => {
+    const page = fs.readFileSync("./data/irl-eco/page.html").toString();
+    console.log(page);
+    expect(noSelectionPageToBody(page)).toEqual({
+      nb_tickets_selector: {
+        quantity: "0",
+      },
+      "selected_tickets_31690": "83009,83010",
+      "tickets_selected": "",
+      "form_build_id": "form-0soa4ZGFEtEuOhBfn2x7xPbUZVGM3CdipJPHn81OEvI",
+      "form_token": "5vMNSg5zQaYnkhDEP6FvZzDdhMCEvHRcXgL_t57qttY",
+      "form_id": "hubber_resale_add_to_cart_form",
+      "_triggering_element_name": "add_pack31690",
+      "_triggering_element_value": "Ajouter à mon panier",
+    });
+  });
+  
