af9e67bedb6df169717e49b241c6fb4aa669c185
Antoine Sauvage <antoine@ovrsea.com>
2023-03-16 18:52:54 +0100

    ok done

--- book/src/main.ts
+++ book/src/main.ts
@@ -2,23 +2,30 @@ import { fetchMainPage } from "./fetch-main-page";
 import { noSelectionPageToBody } from "./page-handler/page-to-form-body";
 import * as fs from "fs"
 
-const token = "SSESSd01ad0e956e3c977e4fabd0c735f147f=uN9K4PFY5ZrjjQsnfz45t1PJ8LpJ4VlXvPL%2CcgzanMfk4CTB";
+const cookie = "SSESSd01ad0e956e3c977e4fabd0c735f147f=uN9K4PFY5ZrjjQsnfz45t1PJ8LpJ4VlXvPL%2CcgzanMfk4CTB";
 const url = "https://tickets.rugbyworldcup.com/fr/revente_france_namibie";
 
 import Fastify from 'fastify'
+import { submitForm } from "./page-handler/submitForm";
 const fastify = Fastify({
   logger: true
 })
 
 
 
-const main = async () => {
+const main = async (url: string) => {
 
-  //const initialPage = await (await fetchMainPage(url, token)).text();
-  //fs.writeFileSync("log.html", initialPage)
-  //const body = noSelectionPageToBody(initialPage)
-  
-  //console.log(body)
+  const initialPage = await (await fetchMainPage(url, token)).text();
+  console.log("initial page fetched")
+  fs.writeFileSync("log.html", initialPage)
+  const body = noSelectionPageToBody(initialPage)
+  console.log(body)
+  const querystring = require("querystring");
+
+let queryString = querystring.stringify(body);
+  const result = await submitForm(url, cookie, queryString)
+  console.log(await result.text())
+
   
 
 };
@@ -30,8 +37,12 @@ fastify.get('/', function (request, reply) {
   })
 
   fastify.post('/', function (request, reply) {
-    console.log(request)
-    reply.send({ hello: 'world' })
+    const message = request.body.message
+    console.log("Message:", message)
+    const url = message.match(/#(.+)#/)[1]
+    console.log("URL:", url)
+    main(url)
+    reply.send({ url })
   })
   
   // Run the server!
--- book/src/page-handler/request.ts
+++ book/src/page-handler/request.ts
@@ -26,9 +26,7 @@ const token =
 
 console.log(body);
 
-const querystring = require("querystring");
 
-let queryString = querystring.stringify(body);
 
 const promise = submitForm(
   "https://tickets.rugbyworldcup.com/fr/revente_angleterre_argentine?ajax_form=1&_wrapper_format=drupal_ajax",
